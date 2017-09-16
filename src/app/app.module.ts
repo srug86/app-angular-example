611961997c1c4b201f88e511d62d9e3f84a45c9f
Sergio de la Rubia <srug86@gmail.com>
2017-09-16 19:55:10 +0200

    Implementa código correspondiente a la Sección 2 del curso.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FechaactualComponent } from './fechaactual/fechaactual.component';
 import { CopyrightComponent } from './copyright/copyright.component';
+import { ViewmodeloComponent } from './viewmodelo/viewmodelo.component';
+import { EjmetodoComponent } from './ejmetodo/ejmetodo.component';
+import { EjpropertybindingComponent } from './ejpropertybinding/ejpropertybinding.component';
+import { EjeventbindingComponent } from './ejeventbinding/ejeventbinding.component';
+import { Ej2waybindingComponent } from './ej2waybinding/ej2waybinding.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     FechaactualComponent,
-    CopyrightComponent
+    CopyrightComponent,
+    ViewmodeloComponent,
+    EjmetodoComponent,
+    EjpropertybindingComponent,
+    EjeventbindingComponent,
+    Ej2waybindingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
